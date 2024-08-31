240b06ccea3be9a8d36316df6dd3c0039a6e3fd6
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-08-31 02:57:17 -0700

    donate

--- app/ayudar/donate/page.js
+++ app/ayudar/donate/page.js
@@ -1,13 +1,14 @@
 export default function Donate() {
-  return (
-    <div style="position:relative;overflow:hidden;height:1200px;width:100%;">
-      <iframe
-        title="Donation form powered by Zeffy"
-        style="position: absolute; border: 0; top:0;left:0;bottom:0;right:0;width:100%;height:100%"
-        src="https://www.zeffy.com/embed/donation-form/3ab5b5ab-dfc3-4373-a391-311962616835"
-        allowpaymentrequest
-        allowTransparency="true"
-      ></iframe>
-    </div>
-  );
-}
+    return (
+      <div style={{ position: "relative", overflow: "hidden", height: "1200px", width: "100%" }}>
+        <iframe
+          title="Donation form powered by Zeffy"
+          style={{ position: "absolute", border: 0, top: 0, left: 0, bottom: 0, right: 0, width: "100%", height: "100%" }}
+          src="https://www.zeffy.com/embed/donation-form/3ab5b5ab-dfc3-4373-a391-311962616835"
+          allowPaymentRequest
+          allowTransparency="true"
+        ></iframe>
+      </div>
+    );
+  }
+  
