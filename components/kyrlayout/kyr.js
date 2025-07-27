7b1a88527c0fb59b27cb9453b0df8d9dda51b693
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2025-07-26 22:37:44 -0700

    addcard

---
+++ components/kyrlayout/kyr.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Image from "next/image";
+import classes from "./page.module.css";
+
+const KYR = ({
+  title,
+  text,
+  flag,
+  imageAlt,
+  reverse,
+  youtubeId,
+  youtubeId2,
+}) => {
+  return (
+    <div>
+      <div className={`${classes.row}`}>
+        {youtubeId != "" && (
+          <div className={`${classes.imageContainer} ${classes.hideOnSmall}`}>
+            <Image
+              className={classes.large}
+              src={flag}
+              alt={imageAlt}
+              width={300}
+              height={300}
+              style={{ marginLeft: "5rem" }}
+            />
+          </div>
+        )}
+        {youtubeId2 != "" && (
+          <div className={classes.imageContainer}>
+            <Image
+              className={classes.large}
+              src={flag}
+              alt={imageAlt}
+              width={300}
+              height={300}
+              style={{ marginLeft: "5rem" }}
+            />
+          </div>
+        )}
+      </div>
+
+      <div className={`${classes.row}`}>
+        {youtubeId != "" && (
+          <div className={`${classes.imageContainer} ${classes.forceTwoColumns}`} >
+            <iframe
+              src={`https://www.youtube.com/embed/${youtubeId}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              className={classes.large}
+            />
+          </div>
+        )}
+        {youtubeId2 != "" && (
+          <div className={`${classes.imageContainer} ${classes.forceTwoColumns}`} >
+            <iframe
+              src={`https://www.youtube.com/embed/${youtubeId2}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              className={classes.large}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default KYR;
