ce55f2d28090e42cdd04842af55a63335414df4e
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2025-09-28 09:34:07 -0500

    ui-fixes

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ import HC from "public/images/hc.png";
 import Esp from "public/images/octclass.png";
 import iphone from "public/images/iphone.png";
 import android from "public/images/android.png";
-import KYR from "public/immigration/kyr.jpg"
+import KYR from "public/immigration/kyr.jpg";
 import AI from "public/classflyers/AI hacks.png";
 import IEPen from "public/classflyers/eng.png";
 import IEPes from "public/classflyers/spa.png";
@@ -18,27 +18,21 @@ import IEPbest from "public/classflyers/EnglishBestPractices.png";
 import IEPbestA from "public/classflyers/ASLBestPractices.png";
 import IS from "public/classflyers/IS.png";
 import LSC from "public/classflyers/LSC.png";
-import Muslim from "public/classflyers/Muslimworkshop.png"
+import Muslim from "public/classflyers/Muslimworkshop.png";
 
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
         <div className={classes.slideshow}>
-          <Image
-            src={logo}
-            width={300}
-            height={300}
-            alt="manos unidas logo"
-          ></Image>
+          <Image src={logo} width={300} height={300} alt="manos unidas logo" />
         </div>
         <div>
           <div className={classes.hero}>
             <h1>Hands United</h1>
-
             <p>
-              Supporting immigrant families families with children who are Deaf
-              or hard of hearing to connect and grow with their child.{" "}
+              Supporting immigrant families with children who are Deaf or hard
+              of hearing to connect and grow with their child.
             </p>
           </div>
           <div className={classes.cta}>
@@ -47,106 +41,115 @@ export default function Home() {
           </div>
         </div>
       </header>
+
       <main className={classes.main}>
         <section className={classes.section}>
           <h2>Download our app!</h2>
           <div>
-            <Link
-              href={
-                "https://apps.apple.com/us/app/hands-united-oregon/id6747975975"
-              }
-            >
-              <Image src={iphone} height={50}></Image>Download for iPhone
+            <Link href="https://apps.apple.com/us/app/hands-united-oregon/id6747975975">
+              <Image src={iphone} width={150} height={50} alt="Download for iPhone" />
+              Download for iPhone
             </Link>
-            <p> </p>
-            <Link
-              href={
-                "https://play.google.com/store/apps/details?id=com.createcommunity.handsunitedoregon&pcampaignid=web_share"
-              }
-            >
-              <Image src={android} height={75}></Image>Download for Android
+            <p></p>
+            <Link href="https://play.google.com/store/apps/details?id=com.createcommunity.handsunitedoregon&pcampaignid=web_share">
+              <Image src={android} width={150} height={75} alt="Download for Android" />
+              Download for Android
             </Link>
           </div>
+
           <h2>Know Your Rights card in Sign Languages!</h2>
-          <p>Here is our Know Your Rights card in multiple sign languages (Click image to go to the page).  This page will continue to grow as videos come in.</p>
-          <Link href={"/kyr"}>
-          <div className={classes.responsiveImageWrapper}><Image src={KYR} width={900}></Image></div></Link>
-          <p>Click <a href={"https://drive.google.com/file/d/16daxkMvOyyFnlocwfj4B471exwoMYYn1/view?usp=sharing"} target="_blank">here</a> to download the printable version.</p>
+          <p>
+            Here is our Know Your Rights card in multiple sign languages (Click
+            image to go to the page). This page will continue to grow as videos
+            come in.
+          </p>
+          <Link href="/kyr">
+            <div className={classes.responsiveImageWrapper}>
+              <Image src={KYR} width={900} height={600} alt="Know Your Rights flyer" />
+            </div>
+          </Link>
+          <p>
+            Click{" "}
+            <a href="https://drive.google.com/file/d/16daxkMvOyyFnlocwfj4B471exwoMYYn1/view?usp=sharing">
+              here
+            </a>{" "}
+            to download the printable version.
+          </p>
+
           <h2>Resources Galore!</h2>
           <p>We are excited to share our resources with you!</p>
+
           <p>
             We have multilingual dictionaries (ASL, English, Spanish, LSM)
             available on Teachers Pay Teachers. (Click image to go to TPT)
-            <Link
-              href={"https://www.teacherspayteachers.com/store/manos-unidas"}
-            >
-              <div className={classes.responsiveImageWrapper}>
-              <Image src={TPT} width={900} height={500}></Image></div>
-            </Link>
           </p>
+          <Link href="https://www.teacherspayteachers.com/store/manos-unidas">
+            <div className={classes.responsiveImageWrapper}>
+              <Image src={TPT} width={900} height={500} alt="Teachers Pay Teachers store" />
+            </div>
+          </Link>
+
           <p>
             We also have our dictionary terms available online in our Spanish
             website. (Click image to go to dictionary.)
-            <Link href={"https://manosunidasor.org/ASL/Diccionario"}>
-            <div className={classes.responsiveImageWrapper}>
-              <Image src={Dict} width={850} height={500}></Image></div>
-            </Link>
           </p>
+          <Link href="https://manosunidasor.org/ASL/Diccionario">
+            <div className={classes.responsiveImageWrapper}>
+              <Image src={Dict} width={850} height={500} alt="Online dictionary" />
+            </div>
+          </Link>
+
           <p>
             We have also made communication boards for use in ICE/immigration
             situations. Please download them and share them as needed.
-            <Link
-              href={
-                "https://drive.google.com/drive/folders/1OTmVwvIq9h2ntIJuCkol6lKGhCsHSPv9?usp=drive_link"
-              }
-            >
-              <div className={classes.responsiveImageWrapper}>
-              <Image src={ICE} width={800} height={500}></Image></div>
-            </Link>
           </p>
-          <h2>Upcoming classes</h2>
-                    <p>
-            We are excited to be soon offering ASL classes in spoken Spanish as well as in spoken Haitian Creole.
+          <Link href="https://drive.google.com/drive/folders/1OTmVwvIq9h2ntIJuCkol6lKGhCsHSPv9?usp=drive_link">
+            <div className={classes.responsiveImageWrapper}>
+              <Image src={ICE} width={800} height={500} alt="ICE communication boards" />
+            </div>
+          </Link>
 
-              <div className={classes.responsiveImageWrapper}>
-              <Image src={HC} width={500} height={500}></Image>
-              <h2></h2>
-              <Image src={Esp} width={500} height={500}></Image></div>
-            
+          <h2>Upcoming Classes</h2>
+          <p>
+            We are excited to be soon offering ASL classes in spoken Spanish as
+            well as in spoken Haitian Creole.
           </p>
-                    <h2>Upcoming workshops</h2>
-                    <p>
-            We are excited to be soon offering several workshops soon.  These include AI tips for DHH to interact with the hearing community, how to work with the Muslim community, IEP interpreting workshops, IEP best practices workshops, Colombian Sign Language and International Sign Language!
+          <div className={classes.responsiveImageWrapper}>
+            <Image src={HC} width={500} height={500} alt="Haitian Creole class flyer" />
+            <Image src={Esp} width={500} height={500} alt="Spanish class flyer" />
+          </div>
 
-              <div className={classes.responsiveImageWrapper}>
-              <Image src={AI} width={500}></Image>
-              <h2></h2>
-              <Image src={IEPen} width={500}></Image>
-              <h2></h2>
-              <Image src={IEPes} width={500}></Image>
-              <h2></h2>
-              <Image src={Muslim} width={500}></Image>
-              <h2></h2>
-              <Image src={IEPbestA} width={500}></Image>
-              <h2></h2>
-              <Image src={IEPbest} width={500}></Image>
-              <h2></h2>
-              <Image src={IS} width={500}></Image>
-              <h2></h2>
-              <Image src={LSC} width={500}></Image></div>
-            
+          <h2>Upcoming Workshops</h2>
+          <p>
+            We are excited to be soon offering several workshops soon. These
+            include AI tips for DHH to interact with the hearing community, how
+            to work with the Muslim community, IEP interpreting workshops, IEP
+            best practices workshops, Colombian Sign Language and International
+            Sign Language!
           </p>
+          <div className={classes.responsiveImageWrapper}>
+            <Image src={AI} width={500} height={500} alt="AI tips flyer" />
+            <Image src={IEPen} width={500} height={500} alt="IEP English flyer" />
+            <Image src={IEPes} width={500} height={500} alt="IEP Spanish flyer" />
+            <Image src={Muslim} width={500} height={500} alt="Muslim workshop flyer" />
+            <Image src={IEPbestA} width={500} height={500} alt="ASL Best Practices flyer" />
+            <Image src={IEPbest} width={500} height={500} alt="English Best Practices flyer" />
+            <Image src={IS} width={500} height={500} alt="International Sign flyer" />
+            <Image src={LSC} width={500} height={500} alt="Colombian Sign Language flyer" />
+          </div>
         </section>
+
         <section className={classes.section}>
           <h2>Request Workshops</h2>
           <p>
             In partnership with Deaf and CODA instructors, we offer workshops on
-            basic foreign signed langauges presented in ASL. To date, we are
+            basic foreign signed languages presented in ASL. To date, we are
             offering Mexican Sign Language (LSM) and Guatemalan Sign Language
             (LENSEGUA). To request other workshops please fill out{" "}
             <a href="https://forms.gle/uMKttFSJQkkbacnw7">this form</a>.
           </p>
         </section>
+
         <section className={classes.section}>
           <h2>Who We Are</h2>
           <p>
@@ -156,7 +159,6 @@ export default function Home() {
             over 230 families across the US. In May 2024, we decided to start
             our non-profit business to offer more services to our families.
           </p>
-
           <p>
             Our board members have over 50 years of experience working with Deaf
             children and immigrant families. We also partner with organizations
