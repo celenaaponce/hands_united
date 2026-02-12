4c5a34ae8efc0f051298b0432059412be54d8515
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2026-02-12 12:24:32 -0800

    updatemetadata

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,56 @@
-import MainHeader from "../components/main-header/main-header"
+import MainHeader from "../components/main-header/main-header";
 import Footer from "../components/main-footer/main-footer";
 import "./globals.css";
 
 export const metadata = {
   title: "Hands United",
   description:
-    "Supporting immigrant families with children who are Deaf or hard of hearing to connect and grow with their child.",
+    "Supporting immigrant families with Deaf and hard of hearing children.",
+  openGraph: {
+    title: "Hands United",
+    description:
+      "Supporting immigrant families with Deaf and hard of hearing children.",
+    url: "https://handsunitedor.org",
+    siteName: "Hands United",
+    images: [
+      {
+        url: "https://handsunitedor.org/images/social-preview.png", // change if needed
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
+const structuredData = {
+  "@context": "https://schema.org",
+  "@type": "NonprofitOrganization",
+  name: "Hands United",
+  url: "https://handsunitedor.org",
+  logo: "https://handsunitedor.org/event-logo.png",
+  sameAs: [
+    "https://www.youtube.com/@handsunitedor",
+    "https://www.instagram.com/handsunitedor",
+    "https://www.facebook.com/handsunitedor",
+    "https://www.linkedin.com/company/hands-united"
+  ]
+};
+
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+        />
+      </head>
       <body>
         <MainHeader />
-        <div className='content-wrapper'>{children}</div>
-        <Footer/>
+        <div className="content-wrapper">{children}</div>
+        <Footer />
       </body>
     </html>
   );
